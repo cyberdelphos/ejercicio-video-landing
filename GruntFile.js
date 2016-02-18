18fc26d8c99788c6e457deee5224ce9c3b50a89b
A.Carrasco <alejandro.carrasco@nearbpo.com>
2016-02-18 14:30:40 -0600

    Ajustando conf de grunt-gh-pages

--- GruntFile.js
+++ GruntFile.js
@@ -26,6 +26,7 @@ module.exports = function (grunt) {
             //tasks: ['concat', 'uglify', 'less']
             tasks: ['less']
         },
+
         'gh-pages': {
             options: {
                 base: 'app'
